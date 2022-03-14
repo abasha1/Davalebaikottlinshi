b75ccfe0062d67dd3388c12cc4d7f4aa9759efb0
abashapervoi <51801315+abasha1@users.noreply.github.com>
2022-03-14 12:45:27 -0700

    Create homework2.kt

---
+++ homework2.kt
@@ -0,0 +1,24 @@
+fun main(args: Array<String>) {
+
+    var num = 121
+
+    var firstint = 0
+
+    var saveint: Int
+
+    val secondint: Int
+
+    secondinteger = num
+
+    while (num != 0) {
+        saveint = num % 10
+        firstint = firstint * 10 + saveint: 
+        num /= 10
+    }
+
+
+    if (secondint == firstint)
+        println(true)
+
+    else
+        println(false)
